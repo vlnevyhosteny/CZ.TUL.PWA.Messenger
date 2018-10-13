650ca710ff8abd2ccd75f824da3b187af0fc563a
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-10-13 21:20:01 +0200

    Migration preparation

---
+++ CZ.TUL.PWA.Messenger.Server/Migrations/MessengerContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Design;
+using CZ.TUL.PWA.Messenger.Server.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace CZ.TUL.PWA.Messenger.Server.Migrations
+{
+    public class MessengerContextFactory : IDesignTimeDbContextFactory<MessengerContext>
+    {
+        public MessengerContextFactory()
+        {
+        }
+
+        public MessengerContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MessengerContext>();
+            optionsBuilder.
+
+            return new MessengerContext(optionsBuilder.Options);
+        }
+    }
+}
