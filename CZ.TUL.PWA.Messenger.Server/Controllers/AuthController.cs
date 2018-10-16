35cfa88eba9611f80a993bbcf4d59ca0b0f24c14
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-10-16 06:30:06 +0200

    Auth controller bug fix

--- CZ.TUL.PWA.Messenger.Server/Controllers/AuthController.cs
+++ CZ.TUL.PWA.Messenger.Server/Controllers/AuthController.cs
@@ -10,43 +10,74 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using CZ.TUL.PWA.Messenger.Server.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace CZ.TUL.PWA.Messenger.Server.Controllers
 {
     [Route("api/auth")]
     public class AuthController : Controller
     {
-        public IConfiguration Configuration { get; }
-
-        public MessengerContext MessengerContext { get; }
-
-        public AuthController(IConfiguration configuration, MessengerContext messengerContext)
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<User> _userManager;
+        
+        public AuthController(IConfiguration configuration, UserManager<User> userManager)
         {
-            this.Configuration = configuration;
-            this.MessengerContext = messengerContext;
+            this._configuration = configuration;
+            this._userManager = userManager;
         }
         
         [HttpPost, Route("login")]
         public async Task<IActionResult> LoginAsync([FromBody] UserViewModel givenUser)
         {
-            if (givenUser == null)
+            if (ModelState.IsValid == false)
             {
-                return BadRequest("User not given");
+                return BadRequest(ModelState);
             }
 
-            return Ok(new { Token = ComposeJwtTokenString() });
+            var identity = await GetClaimsIdentity(givenUser.UserName, givenUser.Password);
+            if (identity == null)
+            {
+                // TODO
+                return BadRequest();
+            }
+
+            return new OkObjectResult(ComposeJwtTokenString());
+        }
+
+        private async Task<User> GetClaimsIdentity(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                return await Task.FromResult<User>(null);
+            }
+
+            // get the user to verifty
+            var userToVerify = await _userManager.FindByNameAsync(userName);
+            if (userToVerify == null)
+            {
+                return await Task.FromResult<User>(null);
+            }
+
+            // check the credentials
+            if (await _userManager.CheckPasswordAsync(userToVerify, password))
+            {
+                return await Task.FromResult(userToVerify);
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return await Task.FromResult<User>(null);
         }
 
         string ComposeJwtTokenString()
         {
-            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["AuthSecret:SecurityKey"]));
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AuthSecret:SecurityKey"]));
             var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
 
             var tokeOptions = new JwtSecurityToken(
-                issuer: Configuration["Auth:Issuer"],
-                audience: Configuration["Auth:Audience"],
+                issuer: _configuration["Auth:Issuer"],
+                audience: _configuration["Auth:Audience"],
                 claims: new List<Claim>(),
-                expires: DateTime.Now.AddMinutes(Int32.Parse(Configuration["Auth:TokenExpiration"])),
+                expires: DateTime.Now.AddMinutes(Int32.Parse(_configuration["Auth:TokenExpiration"])),
                 signingCredentials: signinCredentials
             );
 
