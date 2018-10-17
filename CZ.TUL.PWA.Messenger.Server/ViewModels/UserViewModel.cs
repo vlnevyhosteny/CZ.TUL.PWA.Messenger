5b62e21bf6528f5c64c30cea93d10b6e8e176e3f
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-10-17 11:30:59 +0200

    UserViewModel added

---
+++ CZ.TUL.PWA.Messenger.Server/ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CZ.TUL.PWA.Messenger.Server.ViewModels
+{
+    public class UserViewModel
+    {
+        public UserViewModel(string id, string userName, string name)
+        {
+            this.Id = id;
+            this.UserName = userName;
+            this.Name = name;
+        }
+
+        public string Id
+        {
+            get;
+        }
+
+        public string UserName
+        {
+            get;
+        }
+
+        public string Name 
+        {
+            get;
+        }
+    }
+}
