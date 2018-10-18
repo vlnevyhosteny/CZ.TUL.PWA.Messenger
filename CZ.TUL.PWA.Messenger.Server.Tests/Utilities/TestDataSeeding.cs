724a883b40d37c51df10618396eacdc506442744
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-10-18 22:58:23 +0200

    User Controller testing draft

--- CZ.TUL.PWA.Messenger.Server.Tests/Utilities/TestDataSeeding.cs
+++ CZ.TUL.PWA.Messenger.Server.Tests/Utilities/TestDataSeeding.cs
@@ -23,6 +23,21 @@ namespace CZ.TUL.PWA.Messenger.Server.Tests.Utilities
             var userStore = new UserStore<User>(db);
             userStore.CreateAsync(user);
 
+            var user2 = new User
+            {
+                Name = "testuser2",
+                UserName = "testuser2",
+                NormalizedUserName = "TESTUSER2"
+            };
+
+            var passwordHasher2 = new PasswordHasher<User>();
+            var hash2 = passwordHasher.HashPassword(user, "password");
+
+            user2.PasswordHash = hash2;
+
+            var userStore2 = new UserStore<User>(db);
+            userStore.CreateAsync(user2);
+
             db.SaveChanges();
         }
     }
