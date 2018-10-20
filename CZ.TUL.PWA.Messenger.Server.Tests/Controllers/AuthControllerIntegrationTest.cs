9a99731bc376a14d20be8117239266182d9b0952
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-10-20 21:10:27 +0200

    Should_Success test method of AuthController

--- CZ.TUL.PWA.Messenger.Server.Tests/Controllers/AuthControllerIntegrationTest.cs
+++ CZ.TUL.PWA.Messenger.Server.Tests/Controllers/AuthControllerIntegrationTest.cs
@@ -10,6 +10,10 @@ using System.Text;
 using Xunit.Sdk;
 using CZ.TUL.PWA.Messenger.Server.Model;
 using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
 
 namespace CZ.TUL.PWA.Messenger.Server.Tests.Controllers
 {
@@ -42,6 +46,46 @@ namespace CZ.TUL.PWA.Messenger.Server.Tests.Controllers
             response.EnsureSuccessStatusCode();
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Refresh_ShouldSuccess()
+        {
+            var client = _factory.CreateClient();
+
+            MessengerContext context = this._factory.Server.Host.Services.GetService(typeof(MessengerContext)) as MessengerContext;
+
+            #region Login
+
+            var data = new
+            {
+                UserName = "testuser",
+                Password = "password"
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(data).ToString(),
+                                            Encoding.UTF8, "application/json");
+
+            var loginResponse = await client.PostAsync("/api/auth/login", content);
+            loginResponse.EnsureSuccessStatusCode();
+
+            #endregion
+
+            var stringLoginResponse = await loginResponse.Content.ReadAsStringAsync();
+            var loginJsonResult = new JsonObject(stringLoginResponse);
+
+            var refreshContent = new StringContent(loginJsonResult.Json, Encoding.UTF8, "application/json");
+            var refreshResponse = await client.PostAsync("/api/auth/login", content);
+            refreshResponse.EnsureSuccessStatusCode();
+
+            var stringRefreshResponse = await refreshResponse.Content.ReadAsStringAsync();
+            var refreshJsonResult = JObject.Parse(stringRefreshResponse);
+            var refreshToken = refreshJsonResult.Value<string>("refreshToken");
+
+            User user = context.Users.Single(x => x.UserName == "testuser");
+
+            Assert.True(context.RefreshTokens.Any(x => x.Token == refreshToken
+                                                  && x.UserId == user.Id));
         }
     }
 }
