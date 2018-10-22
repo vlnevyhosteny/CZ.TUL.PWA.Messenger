0169ef9b8cf809d792c9001ec9fc9cc10cf1235c
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-10-22 08:50:29 +0200

    UserController testing draft

--- CZ.TUL.PWA.Messenger.Server.Tests/Controllers/UsersControllerIntegrationTests.cs
+++ CZ.TUL.PWA.Messenger.Server.Tests/Controllers/UsersControllerIntegrationTests.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using Xunit;
-using CZ.TUL.PWA.Messenger.Server.Model;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using CZ.TUL.PWA.Messenger.Server.Model;
+using CZ.TUL.PWA.Messenger.Server.Tests.Utilities;
 using CZ.TUL.PWA.Messenger.Server.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace CZ.TUL.PWA.Messenger.Server.Tests.Controllers
 {
@@ -22,15 +26,63 @@ namespace CZ.TUL.PWA.Messenger.Server.Tests.Controllers
         {
             var client = _factory.CreateClient();
 
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+                await AuthenticationUtilities.GetTestUserAccessTokenAsync(client));
+
             MessengerContext context = _factory.Server.Host.Services.GetService(typeof(MessengerContext)) 
                                                as MessengerContext;
+
+            await context.Users.AddRangeAsync(GenerateUsers(5));
 
             var response = await client.GetAsync("/api/users");
             response.EnsureSuccessStatusCode();
 
-            var users = response.Content.ReadAsAsync(typeof(UserViewModel)).Result as UserViewModel;
+            var users = response.Content.ReadAsAsync(typeof(IEnumerable<UserViewModel>)).Result as IEnumerable<UserViewModel>;
+
+            int usersCount = await context.Users.CountAsync();
 
             Assert.NotNull(users);
+            Assert.Equal(usersCount, users.Count());
+        }
+
+        [Fact]
+        public async Task GetById_ShouldSuccess()
+        {
+            var client = _factory.CreateClient();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+                await AuthenticationUtilities.GetTestUserAccessTokenAsync(client));
+
+            MessengerContext context = _factory.Server.Host.Services.GetService(typeof(MessengerContext))
+                                               as MessengerContext;
+
+            await context.Users.AddRangeAsync(GenerateUsers(5));
+            await context.SaveChangesAsync();
+
+            var dbUser = await context.Users.Skip(2).FirstAsync();
+
+            var response = await client.GetAsync($"/api/users/{dbUser.Id}");
+            response.EnsureSuccessStatusCode();
+
+            var user = response.Content.ReadAsAsync(typeof(UserViewModel)).Result as UserViewModel;
+
+            Assert.NotNull(user);
+            Assert.Equal(dbUser.UserName, user.UserName);
+        }
+
+        private IEnumerable<User> GenerateUsers(int count) 
+        {
+            var result = new User[count];
+
+            for (int i = 0; i < count; i++) 
+            {
+                result[i] = new User
+                {
+                    UserName = $"gentestuser{i}"
+                };
+            }
+
+            return result;
         }
     }
 }
---
+++ CZ.TUL.PWA.Messenger.Server.Tests/Utilities/AuthenticationUtilities.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CZ.TUL.PWA.Messenger.Server.Tests.Utilities
+{
+    public static class AuthenticationUtilities
+    {
+        public static async Task<string> GetTestUserAccessTokenAsync(HttpClient client) 
+        {
+            var data = new
+            {
+                UserName = "testuser",
+                Password = "password"
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(data).ToString(),
+                                            Encoding.UTF8, "application/json");
+
+            var loginResponse = await client.PostAsync("/api/auth/login", content);
+            loginResponse.EnsureSuccessStatusCode();
+
+            var stringLoginResponse = await loginResponse.Content.ReadAsStringAsync();
+            var loginJsonResult = JObject.Parse(stringLoginResponse);
+
+            return loginJsonResult.Value<string>("token");
+        }
+
+    }
+}
