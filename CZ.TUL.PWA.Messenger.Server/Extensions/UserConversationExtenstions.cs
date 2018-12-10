c904fd0f711c3d8d0dcaf01a0e7c7161bcbe1352
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2018-12-10 18:51:17 +0100

    UserConversationViewModel server

---
+++ CZ.TUL.PWA.Messenger.Server/Extensions/UserConversationExtenstions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CZ.TUL.PWA.Messenger.Server.Extensions
+{
+    public class UserConversationExtenstions
+    {
+        public UserConversationExtenstions()
+        {
+        }
+    }
+}
